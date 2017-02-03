6678ba227c80bd0c5f64429a0db16b95c228c2f8
Alexandr Bieliakov <bieliakov.alex@gmail.com>
2017-02-03 08:32:32 +0200

    Получение "тела" xml-тега

--- RegexSearch/main.cpp
+++ RegexSearch/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 int main()
 {
-	string inputString = "<?xml version=\"1.0\" encoding=\"utf - 8\"?>\n<datachunk>\n<data>1   < / data>\n<data>2< / data>\n< / datachunk>\n"; //простой xml
+	string inputString = "<?xml version=\"1.0\" encoding=\"utf - 8\"?>\n<datachunk>\n<data>   1   < / data>\n<data>2< / data>\n< / datachunk>\n"; //простой xml
 	smatch result;
 	//regex expressionData("<\\s*data\\s*>[:alnum:]*<\\s*/\\s*data\\s*>");
-	regex expressionData("<\\s*?data\\s*?>(?:[\x20-\x7E]|[ \t\r\n\v\f])*?<\\s*?/\\s*?data\\s*?>");
+	regex expressionData("<\\s*?data\\s*?>(([\x20-\x7E]|[ \t\r\n\v\f])*?)<\\s*?/\\s*?data\\s*?>");
 
 	regex_search(inputString, result, expressionData);
 
@@ -19,7 +19,7 @@ int main()
 
 	for (int i = 0;i < count;i++)
 	{
-		std::cout << result[0] << std::endl;
+		std::cout << result[i] << std::endl;
 	}
 
 	_getch();
