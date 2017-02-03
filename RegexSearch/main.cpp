f2eb1e53ce8acb17f4a25745db186cabd01780d3
Alexandr Bieliakov <bieliakov.alex@gmail.com>
2017-02-03 08:27:17 +0200

    Поиск xml-тега data

---
+++ RegexSearch/main.cpp
@@ -0,0 +1,27 @@
+#include <regex>
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	string inputString = "<?xml version=\"1.0\" encoding=\"utf - 8\"?>\n<datachunk>\n<data>1   < / data>\n<data>2< / data>\n< / datachunk>\n"; //простой xml
+	smatch result;
+	//regex expressionData("<\\s*data\\s*>[:alnum:]*<\\s*/\\s*data\\s*>");
+	regex expressionData("<\\s*?data\\s*?>(?:[\x20-\x7E]|[ \t\r\n\v\f])*?<\\s*?/\\s*?data\\s*?>");
+
+	regex_search(inputString, result, expressionData);
+
+	int count = result.size();
+	std::cout << "Count of matches: " << count << std::endl;
+
+	for (int i = 0;i < count;i++)
+	{
+		std::cout << result[0] << std::endl;
+	}
+
+	_getch();
+	return 0;
+}
